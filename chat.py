3cbd4c492f5afc99e634c5f2576ca57488af4ab6
Daniel BATISTA <batistadaniel3007@gmail.com>
2026-04-02 16:40:42 +0100

    feat: Adicionados controlos de página

--- chat.py
+++ chat.py
@@ -1,1 +1,19 @@
 import flet as ft
+
+def main(page: ft.Page):
+    chat = ft.Column()
+    new_message = ft.TextField()
+
+    # Envia a mensagem quando o botão send é carregado
+    def send_click(e):
+        chat.controls.append(ft.Text(new_message.value))
+        new_message.value = ""
+
+    # Adiciona a caixa de chat e o campo de texto para a nova mensagem
+    page.add(
+        chat,
+        ft.Row(controls=[new_message, ft.Button("Send", on_click=send_click)]),
+    )
+
+
+ft.run(main)
